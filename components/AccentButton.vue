d339dbc1907e12f3824cbb52ee86d3d3df01fd4b
Knazran <khairulnazran94@gmail.com>
2021-07-25 18:26:22 +0800

    Created PageHero component

--- components/AccentButton.vue
+++ components/AccentButton.vue
@@ -1,16 +1,27 @@
 <template>
-    <button class='rounded-lg bg-mau-secondary-900 text-center text-white w-32 py-1 mx-2 hover:font-bold hover:bg-mau-secondary-950'>
-        {{ text }}
-    </button>
+  <button
+    class="
+      rounded-lg
+      bg-mau-secondary-900
+      text-center text-white
+      w-32
+      py-1
+      mx-2
+      hover:font-bold
+      hover:bg-mau-secondary-950
+    "
+  >
+    {{ text }}
+  </button>
 </template>
 
 <script>
 export default {
-    props: {
-        text: {
-            type: String,
-            required: true
-        },
-    }
+  props: {
+    text: {
+      type: String,
+      required: true,
+    },
+  },
 }
 </script>
--- pages/about-us.vue
+++ pages/about-us.vue
@@ -1,3 +1,13 @@
 <template>
-    <div>asdasdasdas</div>
+    <div>
+        <PageHero 
+            title="About Us"
+            description="Monsters Among Us is a Malaysian youth combating  child sexual abuse (CSA) through advocacy and community involvement."
+            bg-image="kid-hero.jpg"
+        />
+    </div>
+    <!-- bg-gradient-to-r
+      from-mau-primary-500
+      via-mau-primary-400
+      to-mau-primary-300 -->
 </template>
