7d26e95d36db43758ea39a06eb0f6822cf400231
Knazran <khairulnazran94@gmail.com>
2021-08-08 23:37:12 +0800

    Implemented skeletons for pages except The Issues

---
+++ pages/our-story.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <PageHero title="Our Story" description="" bg-image="kid-hero.jpg" />
+
+    <PageSection id="our-story">
+      <h2 class="text-2xl md:text-3xl text-mau-primary-700 font-semibold mb-6 text-center md:text-left">
+        Our Story
+      </h2>
+    </PageSection>
+  </div>
+</template>
+
+<script>
+export default {
+}
+</script>
+
