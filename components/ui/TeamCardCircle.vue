20f4792de35a794345f50ce9a2e5f87b6f4c0e4b
Fahim Aqil <fahimaqil@gmail.com>
2021-08-12 03:49:45 +0800

    Edited description

--- components/ui/TeamCardCircle.vue
+++ components/ui/TeamCardCircle.vue
@@ -1,22 +1,49 @@
 <template>
-
-      <div class="flex flex-col items-center">
-        <!-- Overlay -->
-         <div class="relative overflow-hidden transition duration-300 transform rounded shadow-lg lg:hover:-translate-y-2 hover:shadow-2xl">
-          <img class="object-cover 	h-96 w-96 mb-2 shadow" :src="img" alt="Person" />
-            <div class="absolute cursor-pointer inset-0 flex flex-col justify-center px-2 py-4 text-center transition-opacity duration-300 bg-black bg-opacity-75 opacity-0 hover:opacity-100">
-              <p class="mb-4 text-xs tracking-wide text-gray-400">
-               {{ description }}
-              </p>
-            </div>
-        </div>
-        <!-- End Overlay -->
-        <div class="flex flex-col items-center my-4">
-          <p class="text-lg font-bold">{{ name }}</p>
-          <p class="text-sm text-gray-800">{{ title }}</p>
-        </div>
+  <div class="flex flex-col items-center">
+    <!-- Overlay -->
+    <div
+      class="
+        relative
+        overflow-hidden
+        transition
+        duration-300
+        transform
+        rounded
+        shadow-lg
+        lg:hover:-translate-y-2
+        hover:shadow-2xl
+      "
+    >
+      <img class="object-cover h-80 w-80 mb-2 shadow" :src="img" alt="Person" />
+      <div
+        class="
+          absolute
+          cursor-pointer
+          inset-0
+          flex flex-col
+          justify-center
+          px-3
+          py-4
+          text-center
+          transition-opacity
+          duration-300
+          bg-black bg-opacity-75
+          opacity-0
+          hover:opacity-100
+        "
+      >
+        <h6 class="mb-4 tracking-wide text-left text-gray-400">
+          {{ description }}
+        </h6>
       </div>
-    <!-- </div> -->
+    </div>
+    <!-- End Overlay -->
+    <div class="flex flex-col items-center my-4">
+      <p class="text-lg font-bold">{{ name }}</p>
+      <p class="text-sm text-gray-800">{{ title }}</p>
+    </div>
+  </div>
+  <!-- </div> -->
 </template>
 <script>
 export default {
@@ -32,7 +59,14 @@ export default {
     },
     img: {
       type: String,
-    }
+    },
   },
 }
 </script>
+<style scoped>
+h6 {
+  font-size: 0.72rem;
+  line-height: 0.8rem;
+}
+</style>
+
--- pages/team.vue
+++ pages/team.vue
@@ -1,19 +1,87 @@
 <template>
-  <div class="px-4 py-8 mx-auto sm:max-w-xl md:max-w-full lg:max-w-screen-2xl md:px-24 lg:px-8 ">
+  <div
+    class="
+      px-4
+      py-8
+      mx-auto
+      sm:max-w-xl
+      md:max-w-full
+      lg:max-w-screen-2xl
+      md:px-24
+      lg:px-8
+    "
+  >
     <!-- <PageHero title="Meet The Team" description="" bg-image="kid-hero.jpg" /> -->
 
-      <h2 class="text-2xl md:text-3xl text-mau-primary-700 font-semibold mt-12 sm:mb-24  mb-12 text-center ">
-        MAU Mainboard
-      </h2>
-      <div class="grid gap-10 mx-auto xl:max-w-screen-2xl lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-4">
-        <TeamCardCircle v-for="member in boardMembers" :key="member.name" :name="member.name" :title="member.title" :img="member.image" :description="member.blurb"/>
-      </div>
-      <h2 class="text-2xl md:text-3xl sm:my-24 my-12 text-mau-primary-700 font-semibold  text-center ">
-        Lapor Predator Members
-      </h2>
-      <div class="grid gap-10 mx-auto xl:max-w-screen-2xl lg:max-w-screen-lg sm:grid-cols-2 lg:grid-cols-4">
-        <TeamCardCircle v-for="member in lpMembers" :key="member.name" :name="member.name" :title="member.title" :img="member.image" :description="member.blurb"/>
-      </div>
+    <h2
+      class="
+        text-2xl
+        md:text-3xl
+        text-mau-primary-700
+        font-semibold
+        mt-12
+        sm:mb-24
+        mb-12
+        text-center
+      "
+    >
+      MAU Mainboard
+    </h2>
+    <div
+      class="
+        grid
+        gap-10
+        mx-auto
+        xl:max-w-screen-2xl
+        lg:max-w-screen-lg
+        sm:grid-cols-2
+        xl:grid-cols-4
+        lg:grid-cols-3
+      "
+    >
+      <TeamCardCircle
+        v-for="member in boardMembers"
+        :key="member.name"
+        :name="member.name"
+        :title="member.title"
+        :img="member.image"
+        :description="member.blurb"
+      />
+    </div>
+    <h2
+      class="
+        text-2xl
+        md:text-3xl
+        sm:my-24
+        my-12
+        text-mau-primary-700
+        font-semibold
+        text-center
+      "
+    >
+      Lapor Predator Members
+    </h2>
+    <div
+      class="
+        grid
+        gap-10
+        mx-auto
+        xl:max-w-screen-2xl
+        lg:max-w-screen-lg
+        sm:grid-cols-2
+        xl:grid-cols-4
+        lg:grid-cols-3
+      "
+    >
+      <TeamCardCircle
+        v-for="member in lpMembers"
+        :key="member.name"
+        :name="member.name"
+        :title="member.title"
+        :img="member.image"
+        :description="member.blurb"
+      />
+    </div>
   </div>
 </template>
 
@@ -26,7 +94,7 @@ export default {
 
     return {
       boardMembers,
-      lpMembers
+      lpMembers,
     }
   },
 }
