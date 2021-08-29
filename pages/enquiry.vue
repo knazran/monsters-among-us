6b871d9872b640c3121cffc8392b2b83af34c454
Fahim Aqil <fahimaqil@gmail.com>
2021-08-30 02:57:39 +0800

    Update enquiry.vue

--- pages/enquiry.vue
+++ pages/enquiry.vue
@@ -1,63 +1,171 @@
 <template>
-<div :style="{ backgroundImage: getBackgroundImage}">
+  <div :style="{ backgroundImage: getBackgroundImage }">
+    <div
+      class="
+        py-12
+        bg-scroll
+        rotate-180
+        lg:bg-contain
+        bg-no-repeat
+        lg:bg-right
+        bg-top
+      "
+      style="background-image: url(img//img_bg/gotta_read_this_bg-2.png)"
+    >
+      <PageSection>
+        <PageTitle
+          description="We are here because we care you. Keep in touch with us!"
+          title="Contact Us"
+        />
 
-<div class="py-12 bg-scroll rotate-180 lg:bg-contain bg-no-repeat lg:bg-right bg-top" style="background-image: url(img//img_bg/gotta_read_this_bg-2.png)">
+        <div class="flex py-8">
+          <form
+            class="w-full max-w-lg"
+            name="contact"
+            action=""
+            method="post"
+            netlify
+          >
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <input type="hidden" name="form-name" value="contact" />
 
-    <PageSection>
-             
-             
-<PageTitle description="We are here because we care you. Keep in touch with us!" title="Contact Us"/>
+                <label
+                  class="
+                    block
+                    tracking-wide
+                    text-mau-primary-700 text-base
+                    font-bold
+                    mb-2
+                  "
+                  for="grid-password"
+                >
+                  Name
+                </label>
 
- <div class="flex py-8">
-     
-  <form class="w-full max-w-lg" name="contact" action="" method="post" netlify>
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full px-3">
-    <input type="hidden" name="form-name" value="contact" />
-
-      <label class="block  tracking-wide text-mau-primary-700 text-base font-bold mb-2" for="grid-password">
-        Name
-      </label>
-
-      <input  placeholder="Your name :)" class="appearance-none block w-full bg-gray-200 text-mau-primary-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="nick" type="text">
+                <input
+                  placeholder="Your name :)"
+                  class="
+                    appearance-none
+                    block
+                    w-full
+                    bg-gray-200
+                    text-mau-primary-700
+                    border border-gray-200
+                    rounded
+                    py-3
+                    px-4
+                    mb-3
+                    leading-tight
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
+                  "
+                  id="nick"
+                  type="text"
+                />
+              </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <label
+                  class="
+                    block
+                    tracking-wide
+                    text-mau-primary-700 text-base
+                    font-bold
+                    mb-2
+                  "
+                  for="grid-password"
+                >
+                  E-mail
+                </label>
+                <input
+                  placeholder="jane@example.com"
+                  class="
+                    appearance-none
+                    block
+                    w-full
+                    bg-gray-200
+                    text-mau-primary-700
+                    border border-gray-200
+                    rounded
+                    py-3
+                    px-4
+                    mb-3
+                    leading-tight
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
+                  "
+                  id="email"
+                  type="email"
+                />
+              </div>
+            </div>
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <label
+                  class="
+                    block
+                    tracking-wide
+                    text-mau-primary-700 text-base
+                    font-bold
+                    mb-2
+                  "
+                  for="grid-password"
+                >
+                  Message
+                </label>
+                <textarea
+                  class="
+                    no-resize
+                    appearance-none
+                    block
+                    w-full
+                    bg-gray-200
+                    text-mau-primary-700
+                    border border-gray-200
+                    rounded
+                    py-3
+                    px-4
+                    mb-3
+                    leading-tight
+                    focus:outline-none
+                    focus:bg-white
+                    focus:border-gray-500
+                    h-48
+                    resize-none
+                  "
+                  id="message"
+                ></textarea>
+              </div>
+            </div>
+            <div class="md:flex md:items-center">
+              <div class="md:w-2/3">
+                <input
+                  class="
+                    form-button
+                    rounded-lg
+                    bg-mau-secondary-900
+                    text-center text-white
+                    w-32
+                    py-1
+                    cursor-pointer
+                    hover:font-bold
+                    hover:bg-mau-secondary-950
+                  "
+                  type="submit"
+                  value="Submit"
+                />
+              </div>
+              <div class="md:w-2/3"></div>
+            </div>
+          </form>
+        </div>
+      </PageSection>
     </div>
   </div>
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full px-3">
-      <label class="block  tracking-wide text-mau-primary-700 text-base font-bold mb-2" for="grid-password">
-        E-mail
-      </label>
-      <input  placeholder="jane@example.com" class="appearance-none block w-full bg-gray-200 text-mau-primary-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500" id="email" type="email">
-    </div>
-  </div>
-  <div class="flex flex-wrap -mx-3 mb-6">
-    <div class="w-full px-3">
-      <label class="block  tracking-wide text-mau-primary-700 text-base font-bold mb-2" for="grid-password">
-        Message
-      </label>
-      <textarea class=" no-resize appearance-none block w-full bg-gray-200 text-mau-primary-700 border border-gray-200 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white focus:border-gray-500 h-48 resize-none" id="message"></textarea>
-    </div>
-  </div>
-  <div class="md:flex md:items-center">
-    <div class="md:w-2/3">
-    <input class="form-button rounded-lg
-      bg-mau-secondary-900
-      text-center text-white
-      w-32
-      py-1
-      cursor-pointer	
-      hover:font-bold
-      hover:bg-mau-secondary-950" type="submit" value="Submit" />
-
-    </div>
-    <div class="md:w-2/3"></div>
-  </div>
-</form>
- </div>
-
-    </PageSection>
-</div>
-</div>
 </template>
 
 <script>
