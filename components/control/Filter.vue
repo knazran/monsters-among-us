c203579f4be13e05b052e6420d05f01850e21891
Fahim Aqil <fahimaqil@gmail.com>
2021-12-03 19:21:17 +0800

    FInal Draft v2 #5

--- components/control/Filter.vue
+++ components/control/Filter.vue
@@ -1,49 +1,49 @@
 <template>
-<div>
-      <HeadingControl :title="content.title1" />
+  <div>
+    <HeadingControl :title="content.title1" />
 
-  <div class="max-h-screen  sm:my-0   imgBg " :style="{ backgroundImage: bgImage }">
-    
-    <div class=" flex flex-col md:flex-row py-4 px-6">
-      <!-- Left Side-->
-      <div class="md:w-3/5 mb-2">
-        <div class="">
-          <slot name="left">
-              <div class="flex mt-4  justify-center">
+    <div
+      class="max-h-screen sm:my-0 imgBg"
+      :style="{ backgroundImage: bgImage }"
+    >
+      <div class="flex flex-col md:flex-row py-4 px-6">
+        <!-- Left Side-->
+        <div class="md:w-3/5 mb-2">
+          <div class="">
+            <slot name="left">
+              <div class="flex mt-4 justify-center">
                 <div>
-                           <SimpleThumbnailCard  :image="content.image" />
+                  <img
+                    :src="content.image"
+                    alt="..."
+                    class="object-scale-down w-full border-none rounded h-72"
+                  />
                 </div>
                 <div class="px-5">
                   <nuxt-content
-                    class="
-                      font-bold  prose-md
-                      lg:prose-lg
-                      max-w-none
-                    "
+                    class="font-bold prose-md lg:prose-lg max-w-none"
                     :document="content"
                   />
+                </div>
               </div>
-              </div>
-           
-          </slot>
+            </slot>
+          </div>
         </div>
-      </div>
-      <!-- End Left Side-->
+        <!-- End Left Side-->
 
-      <!-- Right Side with List and button -->
-      <div class="md:w-2/5 lg:px-0 px-8 md:py-0 py-5">
-        <div class="2xl:pr-12 xl:pr-12">
-          <slot name="right">
-            <div class="w-full md:my-8">
-              <ImageCarouselSingle :images="content.carousel2" />
-            </div>
-          </slot>
+        <!-- Right Side with List and button -->
+        <div class="md:w-2/5 lg:px-0 px-8 md:py-0 py-5">
+          <div class="2xl:pr-12 xl:pr-12">
+            <slot name="right">
+              <div class="w-full md:my-8">
+                <ImageCarouselSingle :images="content.carousel2" />
+              </div>
+            </slot>
+          </div>
         </div>
+        <!--End Right side with Pic -->
       </div>
-      <!--End Right side with Pic -->
     </div>
-  </div>
-
   </div>
 </template>
 
@@ -54,7 +54,8 @@ export default {
     content: {
       type: Object,
       required: true,
-    }, bgImage: {
+    },
+    bgImage: {
       type: String,
       required: true,
     },
